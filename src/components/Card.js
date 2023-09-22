bc06e6df81433f525f300bc9b89528d610acb053
thatmichael85 <sabermarionettej@msn.com>
2023-09-22 10:17:52 +0800

    implemented projects

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Box, Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
@@ -7,7 +7,18 @@ const Card = ({ title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <Box borderRadius="md" bg="white" w="100%" h="100%" p={4} color="black">
+      <VStack spacing={8} align="left">
+        <Image src={imageSrc} />
+        <Heading>{title}</Heading>
+        <Text>{description}</Text>
+        <Text>
+          See more <FontAwesomeIcon icon={faArrowRight} size="1x" />
+        </Text>
+      </VStack>
+    </Box>
+  );
 };
 
 export default Card;
--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -5,6 +5,7 @@ import FullScreenSection from "./FullScreenSection";
 const greeting = "Hello, I am Pete!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
+const pic_url = "https://i.pravatar.cc/150?img=7";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
@@ -15,7 +16,12 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-
+    <VStack spacing={4}>
+      <Avatar size="2xl" src={pic_url}></Avatar>
+      <div>{greeting}</div>
+      <Heading>{bio1}</Heading>
+      <Heading>{bio2}</Heading>
+    </VStack>
   </FullScreenSection>
 );
 
